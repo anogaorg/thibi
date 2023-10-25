054bfc98e7880d0149418ea51bd1fb632e3a45b8
Yusuph Mkangara <yusuph.mka@outlook.com>
2023-10-24 23:02:03 -0700

    Move some DB setup to dedicated file (#3)

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ To run what would be the production build, do `yarn build` and `yarn preview`.
 
 For icons, use [Heroicons](https://heroicons.com/). Copying the SVG seems to be working okay so far.
 
+Another icon provider is [Flowbite](https://flowbite.com/icons/).
+
 # SQLite
 I am still new to the JS ecosystem, but so far it seems like to get this to work, even with the NPM module, one still has to copy a bunch of the JS assets to your final deployed assets. This is primarily because I am interested in using the Worker1 Promiser approach to querying. In the past, I used [this client](https://github.com/magieno/sqlite-client-demo/tree/master) to get better egonomics. I do want to fuss with the lower-level APIs a little bit before choosing a higher-level abstraction. I think I spent a lot of time primarily because I don't fully understand how packaging/bundlers/etc. work in Javascript land. It's been a configuration nightmare and will take some getting used to.
 
---
+++ src/database/database.ts
@@ -0,0 +1,26 @@
+import { MAIN_THIBI_DB } from "../constants";
+
+
+async function openDB(sqlite: unknown): Promise<void> {
+    console.info("Opening main database");
+    // @ts-ignore: sqlite client currently has no type
+    await sqlite('open', { filename: MAIN_THIBI_DB, vfs: 'opfs' })
+        .then(() => {
+            console.info("Database successfully opened and connected");
+        })
+}
+
+async function initJobsTable(sqlite: unknown): Promise<void> {
+    // @ts-ignore: sqlite client currently has no type
+    await sqlite('exec', 'CREATE TABLE IF NOT EXISTS jobs (id INTEGER PRIMARY KEY, timestamp TEXT, file_name TEXT, table_identifier TEXT)');
+    // @ts-ignore: sqlite client currently has no type
+    await sqlite('exec', 'CREATE UNIQUE INDEX IF NOT EXISTS table_id_idx ON jobs(table_identifier)');
+}
+
+async function initDB(sqlite: unknown): Promise<void> {
+    console.info("Initializing database using `opfs`");
+    await openDB(sqlite);
+    await initJobsTable(sqlite);
+}
+
+export default initDB;
